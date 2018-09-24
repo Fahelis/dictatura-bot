35b720a0f1086759697cd5f7414e9412f8071925
Fahelis <32023050+Fahelis@users.noreply.github.com>
2018-09-24 08:53:07 +0200

    Update bot.js
    Indent code

--- bot.js
+++ bot.js
@@ -18,43 +18,38 @@ client.on('message', message => {
   	}
 
 
-if(message.content.startsWith(prefix + "reminder")){
-if(message.channel.type === "dm") return;
+    if(message.content.startsWith(prefix + "reminder")) {
+        if(message.channel.type === "dm") {
+            return;
+        }
 
-if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")){return message.reply("**:x: Vous n'avez pas la permission Administrateur").catch(console.error);
+        if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) {
+            return message.reply("**:x: Vous n'avez pas la permission Administrateur").catch(console.error);
+        } else {
+            let args = message.content.split(' ');
+            let time = args[1];
+            let timeofreminder = message.content.slice(2, args.length);
 
-}else{
-
-let args = message.content.split(' ');
-let time = args[1];
-let timeofreminder = message.content.slice(2, args.length);
-
-function reminder (remind, toRemind) {
-
-if(!time){
-message.channel.send("**:x: Erreur format, Correcte usage: `"+ prefix + "reminder <time en secondes !> <votre reminder>`**");
-}else{
-if(message.content.includes("reminder .")){
-
-setInterval(function() {
-
-message.channel.send();
-}, (time * 1000));
-message.channel.send("** J'ai enlevé votre reminder avec succès :wink:**");
-}else{
-
-setInterval(function() {
-
-message.channel.send(message.content.slice(message.content.indexOf(message.content.split(" ")[2])));
-}, (time * 1000));
-
-message.channel.send("** J'ai ajouter votre reminder avec succès ! Tapez `" + prefix + "reminder .` pour l'enlever :wink:**");
-}
-}
-}
-reminder(time, timeofreminder);
-}
-}
+            function reminder (remind, toRemind) {
+                if(!time) {
+                    message.channel.send("**:x: Erreur format, Correcte usage: `"+ prefix + "reminder <time en secondes !> <votre reminder>`**");
+                } else {
+                if(message.content.includes("reminder .")){
+                    setInterval(function() {
+                        message.channel.send();
+                    }, (time * 1000));
+                    message.channel.send("** J'ai enlevé votre reminder avec succès :wink:**");
+                    } else {
+                        setInterval(function() {
+                            message.channel.send(message.content.slice(message.content.indexOf(message.content.split(" ")[2])));
+                        }, (time * 1000));
+                        message.channel.send("** J'ai ajouter votre reminder avec succès ! Tapez `" + prefix + "reminder .` pour l'enlever :wink:**");
+                    }
+                }
+            }
+            reminder(time, timeofreminder);
+        }
+    }
 
 });
 // THIS  MUST  BE  THIS  WAY
