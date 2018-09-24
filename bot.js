f74acbb63228e5001407e2bee4209a4807b41abb
Fahelis <32023050+Fahelis@users.noreply.github.com>
2018-09-24 10:40:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var prefix = "!";
 
 
 client.on('ready', () => {
-    //bot.channels.find("général").send('Je suis là bande de moules !');
+    bot.channels.find("name", "général").send('Je suis là bande de moules !');
 });
 
 client.on('message', message => {
