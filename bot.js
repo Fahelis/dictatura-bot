081da3598a941ac9f8e1aaa05896150ac5e35df9
Fahelis <32023050+Fahelis@users.noreply.github.com>
2018-09-25 13:52:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,7 +132,7 @@ const TARGET_MINUTE = 0; // Minute of the hour from 0 to 59
 
 setInterval(function() {
     client.channels.find('name', 'général').send('Every minute');
-    var d = new Date('NOW');
+    var d = new Date().now();
     //if (2 !== d.getDay()) return;
     //if (13 !== d.getHours()) return;
     //client.channels.find('name', 'général').send('Bon jour, bonne heure');
