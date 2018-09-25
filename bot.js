540ea27736382749acdb0af3f04c009d3293422b
Fahelis <32023050+Fahelis@users.noreply.github.com>
2018-09-25 14:38:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,9 +133,9 @@ const CHECK_EVERY = 5 // In secondes
 
 setInterval(function() {
     var d = new Date();
-    if ('2' !== d.getDay()) return;
-    if ('14' !== d.getHours()) return;
-    client.channels.find('name', 'général').send('Bon jour, bonne heure');
+    if (2 === d.getDay() && 14 === d.getHours()) {
+        client.channels.find('name', 'général').send('Bon jour, bonne heure');
+    }
 }, CHECK_EVERY * 1000); // Check every CHECK_EVERY secondes
 
 /********************** ! End : Timer functionality ! **********************/
