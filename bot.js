49d7f267f409dd123e76b52a1095a9fe3b7e5db7
Fahelis <32023050+Fahelis@users.noreply.github.com>
2018-09-30 23:07:32 +0200

    Update bot.js
    Tests for tweets

--- bot.js
+++ bot.js
@@ -150,6 +150,38 @@ client.on('message', message => {
 	// Kaelly's Id : 202917352378073088 | Dictatura_bot Id : 484996196977344512
     // Ghost_channel dev : 494101417368354816, prod : 494103730594119690
     if ('202917352378073088' === message.member.id && '494101417368354816' === message.channel.id) {
+        message.embeds.forEach((embed) => {
+		message.channel.send('embed (title) : ' + embed.title);
+             embed.fields.forEach((field) => {
+		     message.channel.send('field (name) : ' + field.name);
+		     message.channel.send('field (value) : ' + field.value);
+                 if (field.name.includes('@DOFUSfr')) {
+                     // Then it's a tweet from Dofus
+                     if (!(field.value.includes('maintenance') || field.value.includes('perturbations')
+                           || field.value.includes('connexion') || field.value.includes('correctif')
+                          || field.value.includes('redémarrage') || field.value.includes('réouverture'))) {
+                         message.delete();
+                     } else {
+                         var embed = new Discord.RichEmbed()
+                             .addField(field.name, field.value)
+                             .setColor('WHITE')
+                         client.channels.find('name', 'annonces').send('Je tente une annonce par field.embed');
+                         client.channels.find('name', 'annonces').sendEmbed(field.embed);
+                         client.channels.find('name', 'annonces').send('Je tente une annonce par embed');
+                         client.channels.find('name', 'annonces').sendEmbed(embed);
+                     }
+                     //return;
+                 }
+              });
+             //return;
+         });
+    }
+	
+	
+	// DEV
+	// Kaelly's Id : 202917352378073088 | Dictatura_bot Id : 484996196977344512
+    // Ghost_channel dev : 494101417368354816, prod : 494103730594119690
+    if ('202917352378073088' === message.member.id && '494101417368354816' === message.channel.id) {
         let embed = message.embeds[0];
         if (embed.title.includes('Tweet')) {
 			message.channel.send('Tweet ');
