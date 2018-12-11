13b7d8f7a78e08808186dccd9e9acefbef94f95c
Fahelis <32023050+Fahelis@users.noreply.github.com>
2018-12-11 09:41:24 +0100

    Call the notifications command

--- main.js
+++ main.js
@@ -29,6 +29,10 @@ client.on('message', message => {
 	    if (false === handledCommand) {
 	    	handledCommand = commands.help(messageLC, config, message);
 	    }
+	    
+	    if (false === handleCommand) {
+	    	handleCommand = commands.notifications(config, message);
+	    }
 
 	    if (false === handledCommand) {
     		message.channel.send('**Je suis désolée mais je ne connais pas la commande ' + message.content.substr(2) + '**');
