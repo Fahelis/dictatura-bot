8b52574d811abc8bfe838791a993e931a7f3df49
Antoine Ratisseau <aratisseau@gmail.com>
2019-01-12 19:40:56 +0100

    Create isDirector message and add message permissionDenied in config

--- commands/commands.js
+++ commands/commands.js
@@ -2,62 +2,62 @@ module.exports = {
 	votes: function(message, config, client, Discord)
 	{
 		if(message.content.startsWith(config.prefix + "votes")) {
-			if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) {
-				return message.reply("**:x: Vous n'avez pas la permission Administrateur").catch(console.error);
-			} else {
-				targetChannel = client.channels.find("name", "les_nouveaux");
-	            let args = message.content.split(" ").slice(1);
-				let autoVote = 0 == args.length;
-				// Find all members who have the role "A l'essai"
-				let recruits = message.guild.roles.find("name", "A l'essai").members;
-			    let hasRecruits = false;
-				// TODO. Find a better way
-				recruits.forEach(function(guildMember, guildMemberId) {
-					hasRecruits = true;
-					});
-				if (!hasRecruits && autoVote) {
-				    let roleMeneur = message.guild.roles.find("name", "Meneur");
-				    let roleBD = message.guild.roles.find("name", "Bras droits");
-				    targetChannel.send('Je ne trouve pas la moindre recrue.\n'
-				      + 'Pas de vote cette semaine sauf si des recrues ne sont pas présentes dans le repaire.\n'
-				      + 'Si tel est le cas le '+ roleMeneur +' ou un '+ roleBD +' doit '
-				      +'lancer le vote manuellement');
-			    } else {
-				    let openningVote = (hasRecruits && autoVote) || (!hasRecruits && !autoVote);
-				    if (openningVote) {
-						targetChannel.send('* * * * * * * * * * * * * * * * * Ouverture des votes * * * * * * * * * * * * * * * * *');
-				    }
-				    if (autoVote) {
-					    recruits.forEach(function(guildMember, guildMemberId) {
-						    // Add each member "A l'essai" to args of the command
-						    args.push(guildMember.displayName);
-						});
-				    }
-					let index = 0;
-			    		// Display a vote for each arg of the command
-					for (let arg in args) {
-						var embed = new Discord.RichEmbed()
-							.addField(args[index], "👍 si vous souhaitez intégrer la recrue, 👊 pour la garder à l'essai, 👎 pour l'exclure")
-							.setColor('RED')
-						targetChannel.sendEmbed(embed)
-						.then(async function (message) {
-							// To get the unicode send \emoji in the chat
-							await message.react("👍");
-							await message.react("👊");
-							await message.react("👎");
-						}).catch(function() {
-							console.log("Can't do the vote");
-						});
-						index++;
-					}
-					if (openningVote) {
-						client.channels.find("name", "annonces").send("@everyone Les votes pour l'intégration des recrues sont ouverts");
-					} else {
-						client.channels.find("name", "annonces").send("@everyone Les recrues manquantes ont été ajoutées aux votes");
-					}
-				}
-				message.delete();
+			if (!isDirector(message.guild.member(message.author)))
+			{
+				return message.reply(config.permissionDeniedMessage);
 			}
+			targetChannel = client.channels.find("name", "les_nouveaux");
+            let args = message.content.split(" ").slice(1);
+			let autoVote = 0 == args.length;
+			// Find all members who have the role "A l'essai"
+			let recruits = message.guild.roles.find("name", "A l'essai").members;
+		    let hasRecruits = false;
+			// TODO. Find a better way
+			recruits.forEach(function(guildMember, guildMemberId) {
+				hasRecruits = true;
+				});
+			if (!hasRecruits && autoVote) {
+			    let roleMeneur = message.guild.roles.find("name", "Meneur");
+			    let roleBD = message.guild.roles.find("name", "Bras droits");
+			    targetChannel.send('Je ne trouve pas la moindre recrue.\n'
+			      + 'Pas de vote cette semaine sauf si des recrues ne sont pas présentes dans le repaire.\n'
+			      + 'Si tel est le cas le '+ roleMeneur +' ou un '+ roleBD +' doit '
+			      +'lancer le vote manuellement');
+		    } else {
+			    let openningVote = (hasRecruits && autoVote) || (!hasRecruits && !autoVote);
+			    if (openningVote) {
+					targetChannel.send('* * * * * * * * * * * * * * * * * Ouverture des votes * * * * * * * * * * * * * * * * *');
+			    }
+			    if (autoVote) {
+				    recruits.forEach(function(guildMember, guildMemberId) {
+					    // Add each member "A l'essai" to args of the command
+					    args.push(guildMember.displayName);
+					});
+			    }
+				let index = 0;
+		    		// Display a vote for each arg of the command
+				for (let arg in args) {
+					var embed = new Discord.RichEmbed()
+						.addField(args[index], "👍 si vous souhaitez intégrer la recrue, 👊 pour la garder à l'essai, 👎 pour l'exclure")
+						.setColor('RED')
+					targetChannel.sendEmbed(embed)
+					.then(async function (message) {
+						// To get the unicode send \emoji in the chat
+						await message.react("👍");
+						await message.react("👊");
+						await message.react("👎");
+					}).catch(function() {
+						console.log("Can't do the vote");
+					});
+					index++;
+				}
+				if (openningVote) {
+					client.channels.find("name", "annonces").send("@everyone Les votes pour l'intégration des recrues sont ouverts");
+				} else {
+					client.channels.find("name", "annonces").send("@everyone Les recrues manquantes ont été ajoutées aux votes");
+				}
+			}
+			message.delete();
 			return true;
 		}
 		return false;
@@ -86,41 +86,42 @@ module.exports = {
 
 	gameVote: function(messageLC, config, message)
 	{
-		if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) {
-			return message.reply("**:x: Vous n'avez pas la permission Administrateur").catch(console.error);
-		} else {
-			let currentChannel = message.channel;
-			if (messageLC.startsWith(config.prefix + 'game_vote')) {
-				message.channel.fetchPinnedMessages()
-				.then(async function (messages) {
-					message = messages.first();
-					if (undefined === message) {
-						currentChannel.send('Je suis désolée, je ne trouve aucun message épinglé')
-						return true;
-					}
-					// To get the unicode send \emoji in the chat
-					await message.react("💩");
-					await message.react("👎");
-					await message.react("👊");
-					await message.react("👍");
-					await message.react("😍");
-				})
-				.catch(console.error);
-				return true;
+		if (messageLC.startsWith(config.prefix + 'game_vote')) {
+			if (!isDirector(message.guild.member(message.author)))
+			{
+				return message.reply(config.permissionDeniedMessage);
 			}
-			return false;
+			console.log("gamevote after if");
+			let currentChannel = message.channel;
+			message.channel.fetchPinnedMessages()
+			.then(async function (messages) {
+				message = messages.first();
+				if (undefined === message) {
+					currentChannel.send('Je suis désolée, je ne trouve aucun message épinglé')
+					return true;
+				}
+				// To get the unicode send \emoji in the chat
+				await message.react("💩");
+				await message.react("👎");
+				await message.react("👊");
+				await message.react("👍");
+				await message.react("😍");
+			})
+			.catch(console.error);
+			return true;
 		}
+		return false;
 	},
 
 	officialMember: function(messageLC, config, message, client)
 	{	 
 		//Vérifie que le message commence par !!officialmember
-		if (messageLC.startsWith(config.prefix + "officialmember")) {
+		if (messageLC.startsWith(config.prefix + "official_member")) {
 			 //Vérifie que le lanceur de la commande a bien les droits pour le faire
-			if (!(message.guild.member(message.author).roles.has('532980986540195840') || message.guild.member(message.author).roles.has('533297290849550336'))) {
-				//S'il n'a pas les droits, message d'erreur et fin du code
-				return message.reply("**:x: Vous n'avez pas le rôle nécessaire pour cette demande").catch(console.error); 
-			} 
+			if (!isDirector(message.guild.member(message.author)))
+			{
+				return message.reply(config.permissionDeniedMessage);
+			}
 			//Vérifie qu'un membre du serveur est mentionné
 			let memberToFind = message.mentions.members.first(); 
 			//S'il n'y a pas de membre sur le serveur correspondant au pseudo mentionné
@@ -184,4 +185,13 @@ module.exports = {
 		}
 		return false;
 	}
+
+}
+
+function isDirector(member)
+{
+	if(!(null !== member.roles.find('name', 'Meneur') || null !== member.roles.find('name', 'Bras droits'))) {
+		return false;
+	}
+	return true;
 }
