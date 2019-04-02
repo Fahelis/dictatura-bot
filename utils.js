16b179903a6f7695f15d5111f4941f2275137cb6
aratisseau <antoine.ratisseau@ac-orleans-tours.fr>
2019-04-02 15:42:20 +0200

    Bugfix. make isDirector a function again

--- utils.js
+++ utils.js
@@ -1,7 +1,10 @@
-function isDirector(member)
-{
-	if(!(null !== member.roles.find('name', 'Meneur') || null !== member.roles.find('name', 'Bras droits'))) {
-		return false;
+module.exports = {
+
+	isDirector: function(member)
+	{
+		if(!(null !== member.roles.find('name', 'Meneur') || null !== member.roles.find('name', 'Bras droits'))) {
+			return false;
+		}
+		return true;
 	}
-	return true;
 }
