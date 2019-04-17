b473795ea0472a4906b7c976bb5085552003cd93
Fahelis <32023050+Fahelis@users.noreply.github.com>
2019-04-17 08:02:20 +0200

    test

--- commands/end_votes.js
+++ commands/end_votes.js
@@ -8,94 +8,88 @@ exports.run = function(client, message)
 		return message.reply(config.permissionDeniedMessage);
 	}
 
+	const ids = [];
 	let currentChannel = message.channel;
 	let generalChannel = client.channels.find('name', 'général');
 	let annoncesChannel = client.channels.find('name', 'annonces');
-	currentChannel.fetchPinnedMessages(false)
+	currentChannel.fetchPinnedMessages()
 		.then(function(pinnedMessages) {
 			let hasVote = false;
 			pinnedMessages.forEach(function(pinnedMessage) {
 				if (undefined === pinnedMessage.embeds[0]) {
 					return;
 				}
+				ids.push(pinnedMessage.id);
 				if (!hasVote) {
 					hasVote = true;
 					currentChannel.send('* * * * * * * * * * * * * * * * * Fermeture des votes * * * * * * * * * * * * * * * * *');
 					annoncesChannel.send('**Résultat des votes hebdomadaires**');
 				}
-				let userName = pinnedMessage.embeds[0].fields[0].name;
-				let realMessage;
-				// Delete the cache
-				/*currentChannel.fetchMessages()
-					.then(allMessages => allMessages.sweep(message => pinnedMessages.has(pinnedMessage.id)));*/
-
-				currentChannel.fetchMessage(pinnedMessage.id)
-        			.then(message => console.log('reactions :' + message.reactions))
-        			.catch(console.error);
-
-				let voteResult = voteCount(pinnedMessage, userName);
-
-
-				let user = pinnedMessage.guild.members.find('displayName', userName);
-				switch (voteResult) {
-					case '👍':
-						if (null === user) {
-							annoncesChannel.send(userName + ' fait maintenant partie des membres officiels');
-						} else {
-							generalChannel.send('!!official_member ' + user)
-								.then(function(pinnedMessage) {
-									pinnedMessage.delete();
-								});
-						}
-						break;
-					case '👊':
-						if (null === user) {
-							annoncesChannel.send(userName + ' voit sa période d\'essai prolongée d\'une semaine');
-						} else {
-							annoncesChannel.send(`${userName} voit sa période d\'essai prolongée d\'une semaine`);
-						}
-						break;
-					case '👎':
-						if (null === user) {
-							annoncesChannel.send(userName + ' nous quitte');
-						} else {
-							generalChannel.send('!!kick_recruit ' + user)
-								.then(function(pinnedMessage) {
-									pinnedMessage.delete();
-								});
-						}
-
-						break;
-					default:
-						console.log('Emote non utilisée pour les votes ' + reaction)
-				}
 			});
+			resolve();
+			// resolve(ids);
 		});
+		for (id in ids) {
+			currentChannel.fetchMessage(id)
+				.then(function (voteMessage) {
+					let userName = voteMessage.embeds[0].fields[0].name;
+					let user = message.guild.members.find('displayName', userName);
+					let voteResult = voteCount(voteMessage, userName);
+					console.log(voteResult);
+					switch (voteResult) {
+						case '👍':
+							if (null === user) {
+								annoncesChannel.send(userName + ' fait maintenant partie des membres officiels');
+							} else {
+								generalChannel.send('!!official_member ' + user)
+									.then(function(cmdMessage) {
+										cmdMessage.delete();
+									});
+							}
+							break;
+						case '👊':
+							if (null === user) {
+								annoncesChannel.send(userName + ' voit sa période d\'essai prolongée d\'une semaine');
+							} else {
+								annoncesChannel.send(`${memberToFind.displayName} voit sa période d\'essai prolongée d\'une semaine`);
+							}
+							break;
+						case '👎':
+							if (null === user) {
+								annoncesChannel.send(userName + ' nous quitte');
+							} else {
+								generalChannel.send('!!kick_recruit ' + user)
+									.then(function(cmdMessage) {
+										cmdMessage.delete();
+									});
+							}
+
+							break;
+						default:
+							console.log('Emote non utilisée pour les votes ' + reaction)
+					}
+
+				});
+		}
 }
 
 function voteCount(message, userName)
 {
-	let nbUp = -1
-	let nbNeutral = -1
-	let nbDown = -1
-	console.log(message.embeds[0].reactions);
-	message.reactions.forEach(function(reaction) {
-		console.log(reaction);
-		console.log('👍');
+	for (let reaction in message.reactions) {
 		switch (reaction) {
 			case '👍':
-				nbUp++;
+				nbUp = reaction.count;
 				break;
 			case '👊':
-				nbNeutral++;
+				nbNeutral = reaction.count;
 				break;
 			case '👎':
-				nbDown++;
+				nbDown = reaction.count;
 				break;
 			default:
 				console.log('Emote non utilisée pour les votes ' + reaction)
 		}
-	});
+	}
 
 	message.channel.send(userName + ' a recueilli ' + nbUp + '👍, ' +
 		nbNeutral + '👊 et ' + nbDown + '👎');
