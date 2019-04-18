bc2eabc108fdc024e4c3731635553427f0aa7636
Fahelis <32023050+Fahelis@users.noreply.github.com>
2019-04-18 11:41:02 +0200

    New command clear_messages

---
+++ commands/clear_messages.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const utils = require('../utils');
+
+exports.run = async (client, message, args) => {
+  if (!utils.isDirector(message.member))
+		{
+			return message.reply(config.permissionDeniedMessage);
+		}
+  if(!args[0]) {
+    return message.channel.send('You must specify the number of messages to delete');
+    }
+
+  message.channel.bulkDelete(args[0]).then(() => {
+    message.channel.send(`:pencil2: ${args[0]} messages have been deleted.`).then(msg => msg.delete(5000));
+  });
+} 
