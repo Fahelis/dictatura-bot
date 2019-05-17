3a54240c79ee5349c5c4e0942b6037cf7a85ff7a
Antoine Ratisseau <aratisseau@gmail.com>
2019-05-17 02:05:57 +0200

    Fix. using unexisting variable and add a test

--- commands/kick_recruit.js
+++ commands/kick_recruit.js
@@ -12,7 +12,10 @@ exports.run = function(client, message, args)
 	if (!member) { 
 		return message.channel.send('Impossible de trouver l\'utilisateur');
 	}
-	client.channels.find("name", "annonces").send(`${memberToFind.displayName} a été exclu du repaire`); 
+	if (!member.roles.find("name", "A l'essai")) {
+		return message.channel.send('Cette commande ne peut expulser que les membres du groupe "A l\'essai"');
+	}
+	client.channels.find("name", "annonces").send(`${member.displayName} a été exclu du repaire`); 
 	member.send('Salut\n' +
 		'Les résultats du vote hebdomadaire viennent de tomber et malheureusement ils ne sont pas en ta faveur\n' +
 		'La majorité est contre ton intégration en guilde\nNous sommes désolé que ça n\'ait pas fonctionné' +
