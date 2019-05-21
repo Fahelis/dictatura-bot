ec5881d7471388c7e32fb43e5e8d112a4f660672
Antoine Ratisseau <aratisseau@gmail.com>
2019-05-21 18:59:38 +0200

    Create a log function to log in console, logs channel and logs archive channel

--- utils.js
+++ utils.js
@@ -6,5 +6,12 @@ module.exports = {
 			return false;
 		}
 		return true;
+	},
+	
+	log: function(client, logMessage)
+	{
+		console.log(logMessage);
+		client.channels.find('name', 'iord_logs').send(logMessage);
+		client.channels.find('name', 'iord_logs_archives').send(logMessage);
 	}
 }
