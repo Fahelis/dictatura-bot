ae2788f8bf99e04dd2682d2042b5f1e9cc01eb25
Antoine Ratisseau <aratisseau@gmail.com>
2019-05-21 19:07:17 +0200

    Create a cleanLogs method and plane it every monday morning to modnight

--- utils.js
+++ utils.js
@@ -13,5 +13,21 @@ module.exports = {
 		console.log(logMessage);
 		client.channels.find('name', 'iord_logs').send(logMessage);
 		client.channels.find('name', 'iord_logs_archives').send(logMessage);
+	},
+
+	cleanLogs: function(client)
+	{
+		logsChannel = client.channels.find('name', 'iord_logs');
+	    let compareDate = new Date();
+	    const nbDaysKeep = 7;
+	    compareDate.setDate(compareDate.getDate() - nbDaysKeep);
+
+	    logsChannel.fetchMessages()
+	  		.then(messages => {
+	  			messages.filter(message => message.createdAt < compareDate).deleteAll();
+	  		})
+	  		.catch(error => {
+	  			module.exports.log(client, error);
+	  		});
 	}
 }
